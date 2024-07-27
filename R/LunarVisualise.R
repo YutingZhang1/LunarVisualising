23182e03b83269232652e0cb94d18d0351847d8d
unknown <zyt@smail.nju.edu.cn>
2024-07-28 02:54:10 +0800

    第三次提交，包含第一版完整包

--- R/LunarVisualise.R
+++ R/LunarVisualise.R
@@ -1,3 +1,115 @@
+#'@title Data Convert to LunarVisualise
+#'@description data convert to LunarVisualise
+#'@param prepared_data
+#'@return NULL
+#'@examples LunarVisualise('prepared_data')
+#'@export
+
+LunarVisualise<-function(prepared_data)
+{
+
+
+#月相底图
+library(WeightedTreemaps)
+library(dplyr)
+library(colorspace)
+
+df5 = read.csv("s5.csv")#对照组数据
+df5 <- df5 %>%
+  mutate(city = gsub(" ", "\n", city)) %>%
+  arrange(desc(m)) %>%
+  mutate(re_size = 5)
+
+tm5 <- voronoiTreemap(
+  data = df5,
+  levels = c("prov", "city"),
+  cell_size = "re_size",
+  custom_color = "m",
+  shape = "circle",
+  positioning = c("regular", "clustered_by_area"),
+  maxIteration = 80,
+  error_tol = 0.1,
+  seed = 31
+)
+#调色板：采用了黑白调色代替月相变化
+custom_pal_1 <- sequential_hcl(
+  n = 34,
+  h = c(-46, 78),
+  c = c(0, 0),
+  l = c(75, 82),
+  power = c(0.8, 1),
+  rev = TRUE
+)
+
+# 创建一个新的R绘图设备，并设置大小
+png("月相底图.png", width = 30, height = 30, units = "cm", res = 500)
+#进行绘图
+drawTreemap(
+  tm5,
+  color_palette = custom_pal_1,
+  color_type = "custom_color",
+  color_level = 2,
+  label_size = 2,  # 调整label大小
+  border_size = 2,
+  border_color = gray(0.9),
+  add = TRUE,
+  layout = c(3, 3),
+  position = c(2, 2)
+)
+
+
+drawTreemap(
+  tm5,
+  color_palette = custom_pal_1,
+  color_type = "custom_color",
+  color_level = 2,
+  label_size = 2,  # 调整label大小
+  border_size = 2,
+  border_color = gray(0.9),
+  add = TRUE,
+  layout = c(3, 3), position = c(1, 2)
+)
+
+drawTreemap(
+  tm5,
+  color_palette = custom_pal_1,
+  color_type = "custom_color",
+  color_level = 2,
+  label_size = 2,  # 调整label大小
+  border_size = 2,
+  border_color = gray(0.9),
+  add = TRUE,
+  layout = c(3, 3), position = c(2, 3)
+)
+
+drawTreemap(
+  tm5,
+  color_palette = custom_pal_1,
+  color_type = "custom_color",
+  color_level = 2,
+  label_size = 2,  # 调整label大小
+  border_size = 2,
+  border_color = gray(0.9),
+  add = TRUE,
+  layout = c(3, 3), position = c(3, 2)
+)
+
+drawTreemap(
+  tm5,
+  color_palette = custom_pal_1,
+  color_type = "custom_color",
+  color_level = 2,
+  label_size = 2,  # 调整label大小
+  border_size = 2,
+  border_color = gray(0.9),
+  add = TRUE,
+  layout = c(3, 3), position = c(2, 1)
+)
+
+
+dev.off()
+
+#月相图
 #加载库
 library(WeightedTreemaps)
 library(dplyr)
@@ -85,7 +197,7 @@ saveTreemapPNG(tm4, "tm1_2022.png", 1.6, 7.8, 7.8)  # 2022
 saveTreemapPNG(tm5, "tm1_2023.png", 2.2, 10.0, 10.0) # 2023
 
 # 生成组合图并设置高分辨率
-png("combined_treemap.png", width = 30, height = 30, units = "cm", res = 1000)  
+png("combined_treemap.png", width = 30, height = 30, units = "cm", res = 1000)
 
 # 使用grid来绘制组合图
 grid.newpage()
@@ -98,7 +210,7 @@ img4 <- png::readPNG("tm1_2022.png")
 img5 <- png::readPNG("tm1_2023.png")
 
 # 设置均匀间隔的布局
-pushViewport(viewport(layout = grid.layout(3, 3, 
+pushViewport(viewport(layout = grid.layout(3, 3,
                                            widths = unit(rep(1/3, 3), "npc"),
                                            heights = unit(rep(1/3, 3), "npc"))))
 
@@ -119,3 +231,4 @@ grid.raster(img5, width = unit(10.0 / 10, "npc"), height = unit(10.0 / 10, "npc"
             vp = viewport(layout.pos.row = 3, layout.pos.col = 2))  # 2023 (3,2)
 
 dev.off()
+}
